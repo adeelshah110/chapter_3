a2f0b6827f272705f1d13270e6759d8dc7c925e9
adeelshah110 <himatshah@quest.edu.pk>
2023-04-15 04:17:43 +0300

    changes

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const calculateformEl = document.getElementById("calculateForm");
-
+const resultsEl = document.getElementById("results");
 //making function with event
 const calculateMarks = (event) => {
-  console.log(event);
+  event.preventDefault();
+  const maxMarks = 400;
+
+  const formdata = new FormData(calculateformEl);
+  grab_data = {};
+  formdata.forEach((value, key) => {
+    grab_data[key] = +value;
+  });
+  const totalMarks =
+    grab_data.science + grab_data.maths + grab_data.sindhi + grab_data.English;
+  const perc = (totalMarks / maxMarks) * 100;
+
+  resultsEl.innerText = `You have got ${totalMarks} from ${maxMarks} your percentage is ${perc}%`;
 };
